b6681e4ce86e3082cebd9ab8df2628583ccd2fb8
Edson Tégila Gonçalves <etegila@gmail.com>
2019-04-12 22:22:10 -0300

    DEBUG env setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const logger = process.env.DEBUG ? console.log : null;
+
 const port = process.env.PORT || 3000;
 const io = require("socket.io")(port);
 
@@ -8,45 +10,45 @@ const crypt = require("common")();
 crypt.init_keypair();
 
 io.sockets.on("connection", function(socket) {
-  console.log("connected");
+  logger("connected");
 
   socket.on("join", function(collection) {
-    console.log("joining room", collection);
+    logger("joining room", collection);
     socket.join(collection);
   });
 
   socket.on("leave", function(collection) {
-    console.log("leaving room", collection);
+    logger("leaving room", collection);
     socket.leave(collection);
   });
 
   // stage 1
   socket.on("message_hash", (message_hash) => {
-    console.log(`receiving message_hash: ${message_hash}`);
+    logger(`receiving message_hash: ${message_hash}`);
     const nonce = Math.random();
 
     const message_hash_nonce = crypt.hash_message({ message_hash, nonce });
-    console.log(`message_hash_nonce: ${message_hash_nonce}`);
+    logger(`message_hash_nonce: ${message_hash_nonce}`);
 
     socket.once(message_hash_nonce, (payload) => {
-      console.log(`receiving data: ${payload}`);
+      logger(`receiving data: ${payload}`);
       console.dir(payload);
       const payload_hash = crypt.hash_message(payload);
-      console.log(`payload_hash: ${payload_hash}`);
+      logger(`payload_hash: ${payload_hash}`);
 
       j2m
       .exec(payload.message)
       .then(ret => {
         // stage 3
         socket.emit(payload_hash, ret);
-        console.log(`sending return: ${ret}`);
+        logger(`sending return: ${ret}`);
         // broadcast if it's a save, update or delete
         // io.sockets.in(data.collection).emit("message", data);
       })
-      .catch(console.log);
+      .catch(logger);
     });
     // stage 2
     socket.emit(message_hash, nonce);
-    console.log(`sending nonce: ${nonce}`);
+    logger(`sending nonce: ${nonce}`);
   });
 });
